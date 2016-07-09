5794b280c58cebdad8c169b67a3c80053d50aa8b
Igor Soloydenko <soloydenko@gmail.com>
2016-07-09 01:15:04 -0700

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Prosecco")]
+[assembly: AssemblyDescription("Simplify SQL execution")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TheOtherGuy")]
+[assembly: AssemblyProduct("Prosecco")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Igor Soloydenko 2016")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3254e9ae-136d-44a7-90da-961ac92678a8")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Used with NuGet
+[assembly: AssemblyInformationalVersion("1.0.0")]
---
+++ SqlClient.cs
@@ -0,0 +1,65 @@
+﻿using Peppermint.Dictionaries;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Prosecco
+{
+    public class SqlClient
+    {
+        private readonly string connectionString;
+
+        public SqlClient(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public Task<int> ExecuteNonQueryAsync(
+            string queryText,
+            IDictionary<string, object> parameters = null)
+        {
+            return ExecuteAsync(queryText, parameters, sqlCmd => sqlCmd.ExecuteNonQueryAsync());
+        }
+
+        public Task<T> ExecuteReaderAsync<T>(
+            string queryText,
+            Func<SqlDataReader, T> run)
+        {
+            return ExecuteReaderAsync(queryText, null, run);
+        }
+
+        public Task<T> ExecuteReaderAsync<T>(
+            string queryText,
+            IDictionary<string, object> parameters,
+            Func<SqlDataReader, T> readResults)
+        {
+            return ExecuteAsync(queryText, parameters, async sqlCmd => readResults(await sqlCmd.ExecuteReaderAsync()));
+        }
+
+        private async Task<T> ExecuteAsync<T>(
+            string queryText,
+            IDictionary<string, object> parameters,
+            Func<SqlCommand, Task<T>> readResults)
+        {
+            parameters = parameters.NullToEmpty();
+
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                var command = sqlConnection.CreateCommand();
+                command.CommandType = CommandType.Text;
+                command.CommandText = queryText;
+
+                foreach (var parameterPair in parameters)
+                {
+                    command.Parameters.AddWithValue(parameterPair.Key, parameterPair.Value);
+                }
+
+                return await readResults(command);
+            }
+        }
+    }
+}
