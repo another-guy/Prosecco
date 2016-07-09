1d8d25092863822f52b48f77a8bf2851f8a2d0df
Igor Soloydenko <soloydenko@gmail.com>
2016-07-09 15:27:29 -0700

    Add unit test for ExecuteNonQueryAsyncFlowIsCorrect

---
+++ Prosecco.Tests/SqlClientTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using NSubstitute;
+using Prosecco;
+using Xunit;
+
+namespace Prosecco.Tests
+{
+    public class SqlClientTest
+    {
+        private readonly string connectionString = @"FAKE";
+        private readonly SqlClient sut;
+
+        private readonly IDbConnection connection;
+        private readonly SqlCommand dbCommand;
+        const int nonQueryCommandAffectedRows = 1;
+
+        public SqlClientTest()
+        {
+            connection = Substitute.For<IDbConnection>();
+            dbCommand = new SqlCommand();
+            Func<SqlCommand, Task<int>> nonQueryExecutor = command =>
+            {
+                Assert.True(
+                    command == dbCommand,
+                    $"Expected command '{dbCommand}' was '{command}'");
+
+                return Task.FromResult(nonQueryCommandAffectedRows);
+            };
+
+            sut = new SqlClient(
+                connectionString,
+                received =>
+                {
+                    Assert.True(
+                        received == connectionString,
+                        $"Expected connection string '{connectionString}' was '{received}'");
+
+                    return connection;
+                },
+                nonQueryExecutor);
+        }
+
+        [Fact]
+        public async void ExecuteNonQueryAsyncFlowIsCorrect()
+        {
+            // Arrange
+            var queryText = "query...";
+            var parameters = new Dictionary<string, object>
+            {
+                { "a", "a" },
+                { "b", 2 }
+            };
+
+            connection.CreateCommand().Returns(dbCommand);
+
+            // Act
+            var affectedRows = await sut.ExecuteNonQueryAsync(queryText, parameters);
+
+            // Assert
+            connection.Received(1).Open();
+
+            Assert.Equal(queryText, dbCommand.CommandText);
+            foreach (var parameter in parameters)
+            {
+                Assert.Equal(parameter.Value, dbCommand.Parameters[parameter.Key].Value);
+            }
+
+            Assert.Equal(nonQueryCommandAffectedRows, affectedRows);
+        }
+
+
+        [Fact]
+        public async void ExecuteReaderAsyncFlowIsCorrect()
+        {
+            // Arrange
+            var queryText = "query...";
+            var parameters = new Dictionary<string, object>
+            {
+                { "a", "a" },
+                { "b", 2 }
+            };
+
+            connection.CreateCommand().Returns(dbCommand);
+
+            var expectedResult = new List<string> { "A", "B" };
+
+            Func<SqlDataReader, List<string>> reader = _ => expectedResult;
+
+            // Act
+            var affectedRows = await sut.ExecuteReaderAsync(queryText, parameters, reader);
+
+            // Assert
+            connection.Received(1).Open();
+
+            Assert.Equal(queryText, dbCommand.CommandText);
+            foreach (var parameter in parameters)
+            {
+                Assert.Equal(parameter.Value, dbCommand.Parameters[parameter.Key].Value);
+            }
+
+            Assert.Equal<List<string>>(expectedResult, affectedRows);
+        }
+    }
+}
---
+++ Prosecco/SqlClient.cs
@@ -0,0 +1,95 @@
+﻿using Peppermint.Dictionaries;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Prosecco
+{
+    public class SqlClient
+    {
+        private static readonly Func<string, IDbConnection> DefaultConnectionCreator =
+            connectionString => new SqlConnection(connectionString);
+
+        private static readonly Func<SqlCommand, Task<int>> DefaultNonQueryExecutor =
+            dbCmd => dbCmd.ExecuteNonQueryAsync();
+
+        // private static readonly Task<SqlDataReader> DefaultReaderExecutor;
+
+
+        private readonly Func<IDbConnection> createConnection;
+        private readonly Func<SqlCommand, Task<int>> nonQueryExecutor;
+        // private readonly Task<SqlDataReader> readerExecutor;
+
+        public SqlClient(string connectionString)
+            : this(connectionString, cs => new SqlConnection(cs))
+        {
+        }
+
+        public SqlClient(
+            string connectionString,
+            Func<string, IDbConnection> connectionCreator = null,
+            Func<SqlCommand, Task<int>> nonQueryExecutor = null)
+        {
+            this.createConnection =
+                () => connectionCreator != null ?
+                    connectionCreator(connectionString) :
+                    DefaultConnectionCreator(connectionString);
+
+            this.nonQueryExecutor = nonQueryExecutor ?? DefaultNonQueryExecutor;
+
+            // this.readerExecutor = readerExecutor ?? DefaultReaderExecutor;
+        }
+
+        public Task<int> ExecuteNonQueryAsync(
+            string queryText,
+            IDictionary<string, object> parameters = null)
+        {
+            return ExecuteAsync(queryText, parameters, nonQueryExecutor);
+        }
+
+        public Task<T> ExecuteReaderAsync<T>(
+            string queryText,
+            Func<SqlDataReader, T> run)
+        {
+            return ExecuteReaderAsync(queryText, null, run);
+        }
+
+        public Task<T> ExecuteReaderAsync<T>(
+            string queryText,
+            IDictionary<string, object> parameters,
+            Func<SqlDataReader, T> readResults)
+        {
+            return ExecuteAsync(queryText, parameters, async dbCmd => readResults(await dbCmd.ExecuteReaderAsync()));
+        }
+
+        private async Task<T> ExecuteAsync<T>(
+            string queryText,
+            IDictionary<string, object> parameters,
+            Func<SqlCommand, Task<T>> readResults)
+        {
+            parameters = parameters.NullToEmpty();
+
+            using (IDbConnection dbConnection = createConnection())
+            {
+                dbConnection.Open();
+
+                var command = (SqlCommand)dbConnection.CreateCommand();
+                command.CommandType = CommandType.Text;
+                command.CommandText = queryText;
+
+                foreach (var parameterPair in parameters)
+                {
+                    var parameter = command.CreateParameter();
+                    parameter.ParameterName = parameterPair.Key;
+                    parameter.Value = parameterPair.Value;
+
+                    command.Parameters.Add(parameter);
+                }
+
+                return await readResults(command);
+            }
+        }
+    }
+}
